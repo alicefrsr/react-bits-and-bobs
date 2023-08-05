9f4c45017a0ad18887482b39155cecbd0424c576
alicefrsr <alicefrsr@gmail.com>
2023-08-05 22:27:16 +0200

    GetAdvice component: adding spinner, fixing count and css

--- src/components/adviceAPI/GetAdvice.jsx
+++ src/components/adviceAPI/GetAdvice.jsx
@@ -1,20 +1,25 @@
 import { useEffect, useState } from 'react';
 import styles from './GetAdvice.module.css';
-import BeatLoader from 'react-spinners/BeatLoader';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 export default function App() {
   const [advice, setAdvice] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
   const [count, setCount] = useState(-1);
 
   async function getAdvice() {
-    const res = await fetch('https://api.adviceslip.com/advice', {
-      cache: 'no-store',
-    });
-    const data = await res.json();
-
-    setAdvice(data.slip.advice);
-    setCount(c => c + 1);
-    // not setCount(count + 1): when the new state depends on a previous state it's best practice to pass in the previous state then update it.
+    try {
+      setIsLoading(true);
+      const res = await fetch('https://api.adviceslip.com/advice', {
+        cache: 'no-store',
+      });
+      const data = await res.json();
+      setIsLoading(false);
+      setAdvice(data.slip.advice);
+      setCount(c => c + 1); // not setCount(count + 1): when the new state depends on a previous state it's best practice to pass in the previous state then update it.
+    } catch (error) {
+      console.log('Something went wrong:', error.message);
+    }
   }
 
   useEffect(() => {
@@ -25,7 +30,11 @@ export default function App() {
     <div className={styles.app}>
       <div className={styles.container}>
         <Message count={count} />
-        <h1 className={styles.advice}>{advice}</h1>
+        <CardBg
+          advice={advice}
+          isLoading={isLoading}>
+          {isLoading ? <Loading /> : <h1 className={styles.advice}>{advice}</h1>}
+        </CardBg>
         <button
           className={styles.btn}
           onClick={getAdvice}>
@@ -37,20 +46,20 @@ export default function App() {
 }
 
 function Message({ count }) {
-  return <p className={styles.number}>Advice number {count}:</p>;
+  return <p className={styles.count}> {count > 0 && `Advice number ${count}:`}</p>;
 }
 
 function CardBg({ children }) {
-  return <main className={'bg-card'}>{children}</main>;
+  return <main className={styles.cardBg}>{children}</main>;
 }
 
 function Loading() {
   return (
     <CardBg>
-      <section className='spinner'>
-        <BeatLoader
-          color={'#0079ff'}
-          size={20}
+      <section>
+        <ClipLoader
+          color={'#3d3d3d'}
+          size={40}
           aria-label={'Loading spinner'}
         />
       </section>
